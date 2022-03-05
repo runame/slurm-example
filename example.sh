943b8c37e6a920a0ce28b47e7a6ea5f3dc353d86
runame <runa.eschenhagen@student.uni-tuebingen.de>
2022-03-05 09:06:44 +0100

    Decrease job array size

--- example.sh
+++ example.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:rtx2080ti:1            # type and number of gpus
 #SBATCH --time=01:00:00                   # job will be cancelled after max. 72h
 #SBATCH --output=example_%A_%a.out        # name of the output file
-#SBATCH --array=1-5                       # separate job for all values in {1,2,3,4,5}
+#SBATCH --array=1-2                       # separate job for all values in {1,2}
 
 # print info about current job
 scontrol show job $SLURM_JOB_ID
